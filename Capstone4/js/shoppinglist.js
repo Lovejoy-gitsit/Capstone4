ab8a6c8ea331d906495717aa5ea14be8932fa170
Lovejoy-gitsit <26442317+Lovejoy-gitsit@users.noreply.github.com>
2018-05-21 08:49:17 -0400

    first commit

---
+++ Capstone4/js/shoppinglist.js
@@ -0,0 +1,14 @@
+﻿$("button").click(function ()
+{
+    var total = Number($("#price span").html());
+    var priceOfItem = Number($(this).siblings(".prices").attr("data_value"));
+    total += priceOfItem; 
+    $("#price span").html(total.toFixed(2));
+    
+
+    //increments the cart quantity everytime add to cart is clicked
+    var quantity = Number($("#select span").html());
+    quantity++;
+    $("#select span").html(quantity);
+
+});
